e188a56d93e9678757111aa90f2648b77e85a42f
wanglinghan <836867547@qq.com>
2019-11-28 09:01:23 +0800

    Adding new modules

---
+++ modules/dirlister.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+"""
+File: dirlister.py
+Author: Rock Johnson
+"""
+import os
+
+
+def run():
+    print('[*] In dirlister module.')
+
+    return str(os.listdir('.'))
---
+++ modules/environment.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+"""
+File: environment.py
+Author: Rock Johnson
+"""
+import os
+
+
+def run():
+    print('[*] In environment module.')
+
+    return str(os.environ)
